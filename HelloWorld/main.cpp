3f67dc78740559bd4f17df72a45737e0bce6b7d3
Garrett Burns <garehburns@users.noreply.github.com>
2020-09-15 11:55:10 -0500

    Add files via upload
    Introductory "Hello World" program for C++

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+// This program simply displays "Hello, world!" in the
+// output window.
+
+#include <iostream>
+using namespace std;
+
+// This is the main function, where the program starts.
+int main() // Defines the main function
+{
+	cout << "Hello, World!\n";
+    return 0; // Allways end it in 0, just for reasons
+}
+
